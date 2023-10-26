a5d1aa3f05f6269091cd13f7908a1f8af7a7619f
tchi-sama <tchisama41@gmail.com>
2023-10-26 07:37:35 -0700

    handel err and loading in the sign up

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -2,7 +2,7 @@
 import EmailInput from '@/components/EmailInput'
 import PasswordInput from '@/components/PasswordInput'
 import { Button } from '@nextui-org/button'
-import { Divider, Input } from '@nextui-org/react'
+import { Code, Divider, Input, Spinner } from '@nextui-org/react'
 import React from 'react'
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '@/firebase'
@@ -15,21 +15,33 @@ function Page({}: Props) {
   
   const router = useRouter()
 
+  const [name, setName] = React.useState("")
   const [email, setEmail] = React.useState("")
   const [password, setPassword] = React.useState("")
   const [username, setUsername] = React.useState("")
+  const [err , setErr] = React.useState("")
+  const [loading, setLoading] = React.useState(false)
 
   const CreateUser = ()=>{
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      router.push("/chat")
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
+    setLoading(true)
+    if(name && email && password && username){
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user)
+        router.push("/chat")
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErr(errorMessage)
+        setLoading(false)
+        // ..
+      });
+    }else{
+      setErr("Please fill all the fields")
+        setLoading(false)
+    }
   }
 
 
@@ -39,6 +51,15 @@ function Page({}: Props) {
         <div className='w-full text-forground-900 flex flex-col gap-2 max-w-[400px]'>
             <h1 className='text-2xl'>Sign up</h1>
             <Divider className='my-8'/>
+            <Input
+              value={name}
+              onInput={(e)=>setName((e.target as HTMLInputElement).value)}
+              isClearable
+              type="email"
+              label={"name"}
+              variant="bordered"
+              className=""
+            />
             <EmailInput value={email} onInput={setEmail} label='Email'/>
             <PasswordInput value={password} onInput={setPassword} label='New Password'/>
             <Input
@@ -46,11 +67,26 @@ function Page({}: Props) {
               onInput={(e)=>setUsername((e.target as HTMLInputElement).value)}
               isClearable
               type="email"
-              label={"Username"}
+              label={"Choose Username"}
               variant="bordered"
               className=""
             />
-            <Button onClick={CreateUser} size='lg' color='secondary'>Sign Up</Button>
+            <Button disabled={loading} onClick={CreateUser} size='lg' color='secondary'>
+              {
+                loading ?
+                <>
+               <Spinner color='white' size='sm'/>
+               Loading
+                </>
+                :
+                <>Sign Up</>
+
+              }
+              </Button>
+            {
+              err && 
+              <p className="text-danger">{err}</p>
+            }
         </div>
     </div>
   )
